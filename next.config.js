c6aea157516decd51f8b4942e89af0ff738df24d
ragab0 <legendragab@gmail.com>
2023-10-05 20:27:51 +0300

    Separting API - SRP single res principle

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
         protocol: 'http',
         hostname: 'localhost',
         port: '5000',
-        pathname: '/api/images/**',
+        pathname: '/api/public/images/**',
       },
     ],
   },
